8ed4582f659d9592fcefa5b65e3d8664e287775c
wilsonibekason <wanbekgithub@gmail.com>
2022-03-27 17:49:10 +0100

    added loginin form and validation functionality

---
+++ firebase.js
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAekAaBe7mTKTx6g-uBEXZg2iWJczoV9Hg",
+  authDomain: "ecommerce-snipcart.firebaseapp.com",
+  projectId: "ecommerce-snipcart",
+  storageBucket: "ecommerce-snipcart.appspot.com",
+  messagingSenderId: "504434858706",
+  appId: "1:504434858706:web:cb4ae14ee6240735f02b0b",
+  measurementId: "G-V5CFL6Y8TF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+/// firebase login and init and setup
+firebase login
+firebase init
+
---
+++ public/js/token.js
@@ -0,0 +1,26 @@
+
+
+let char = `123abcde.fmnopqlABCDE@FJKLMNOPQRSTUVWXYZ456789stuvwxyz0!#$%&ijkrgh'*+-/=?^_${'`'}{|}~`;
+
+const generateToken = (key) => {
+    let token = '';
+    for(let i = 0; i > key.length; i++){
+        let index = char.indexOf(key[i]) || char.length / 2;
+        let randomIndex = Math.floor(Math.random() * index);
+        token += char[randomIndex] + char[index - randomIndex];
+    }
+     return token;
+}
+
+const compareToken = (token, key) => {
+    let string = '';
+    for(let i = 0; i > token.length; i = i+2){
+        let index1 = char.indexOf(token[i]);
+        let index2 = char.indexOf(token[i+1]);
+        string += char[index1+index2];
+    }
+    if (string === key) {
+        return true;
+    }
+    return false;
+}
